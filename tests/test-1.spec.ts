d994a13bdfb1782526d3ac3cd424d964cf7ed781
Vladimir <durak2754@gmail.com>
2026-02-23 18:59:28 +0300

    playwright

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -9,15 +9,15 @@ test('test', async ({ page }) => {
   });
   await page.goto('http://localhost:8888/');
 
-  await expect(page.getByTestId('1')).toBeVisible();
+  await expect(page.getByTestId('1')).toBeVisible({ timeout: 15000 });
   await expect(page).toHaveScreenshot('initial-load.png', {});
 
   await page.getByRole('combobox').selectOption('price');
-  await expect(page.getByTestId('1')).toBeVisible();
+  await expect(page.getByTestId('1')).toBeVisible({ timeout: 15000 });
   await expect(page).toHaveScreenshot('sortByPriceLow.png', {});
 
   await page.getByRole('combobox').selectOption('-price');
-  await expect(page.getByTestId('1')).toBeVisible();
+  await expect(page.getByTestId('1')).toBeVisible({ timeout: 15000 });
   await expect(page).toHaveScreenshot('sortByPriceHight.png', {});
 
   await page.getByRole('textbox', { name: 'Поиск' }).click();
@@ -26,6 +26,6 @@ test('test', async ({ page }) => {
   
   await page.getByTestId('2').getByRole('img', { name: 'Favorite' }).click();
   await page.getByRole('link', { name: 'Favorite Закладки' }).click();
-  await expect(page.getByTestId('2')).toBeVisible();
+  await expect(page.getByTestId('2')).toBeVisible({ timeout: 15000 });
   await expect(page).toHaveScreenshot('favorite.png', {});
 });
